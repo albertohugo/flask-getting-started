50d046aff2c4bcbfaaaddaab8489b89789824e86
albertohugo <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>
2021-11-05 16:55:42 +0000

    Adding cards

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from model import db
 
 app = Flask(__name__)
 
@@ -8,3 +9,11 @@ def welcome():
     return render_template("welcome.html",
                            message="Here's a message from the view"
                            )
+
+@app.route('/card')
+def card_view():
+    card = db[0]
+    return render_template("card.html",
+                           card=card,
+                           )
+
